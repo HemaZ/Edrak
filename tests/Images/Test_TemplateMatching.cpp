acb794c49b8d3727f9afe4e72ea7873d8c59ae82
Ibrahim Essam <ibrahim.essam@avelabs.com>
2022-02-06 23:00:06 +0000

    renaming tests for ctest to pass

--- tests/Images/Test_TemplateMatching.cpp
+++ tests/Images/Test_TemplateMatching.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
-TEST_CASE("Template Dimensions Validation", "[Edrak::Images::matchTemplate()") {
+TEST_CASE("Template Dimensions Validation", "Edrak::Images::matchTemplate()") {
 
   cv::Mat img(256, 256, CV_8UC1);
   cv::Mat templ(512, 512, CV_8UC1);
@@ -31,18 +31,18 @@ TEST_CASE("Template Dimensions Validation", "[Edrak::Images::matchTemplate()") {
       img, templ, res, Edrak::Images::TemplateMatchingMethod::CC_NORMALIZED));
 }
 
-TEST_CASE("Results Dimensions Validation", "[Edrak::Images::matchTemplate()") {
+TEST_CASE("Results Dimensions Validation", "Edrak::Images::matchTemplate()") {
 
   cv::Mat img(512, 512, CV_8UC1);
   cv::Mat templ(64, 64, CV_8UC1);
   cv::Mat res;
   Edrak::Images::matchTemplate(
       img, templ, res, Edrak::Images::TemplateMatchingMethod::CC_NORMALIZED);
-  REQUIRE(res.cols == 449);
-  REQUIRE(res.rows == 449);
+  REQUIRE(res.cols == 512);
+  REQUIRE(res.rows == 512);
 }
 
-TEST_CASE("Filter2D", "[Edrak::Images::matchTemplate()") {
+TEST_CASE("Filter2D", "Edrak::Images::matchTemplate()") {
 
   cv::Mat img = cv::imread("../tests/2011_09_26/2011_09_26_drive_0002_sync/"
                            "image_00/data/0000000000.png",
--- tests/MyTests.cpp
+++ tests/MyTests.cpp
@@ -11,7 +11,7 @@
 #include <type_traits>
 
 // This tests the output of the `Edrak::IO::GetFiles` function
-TEST_CASE("correct files are returned", "[Edrak::IO::GetFiles]") {
+TEST_CASE("correct files are returned", "Edrak::IO::GetFiles") {
   std::string glob_pattern{
       "../tests/2011_09_26/2011_09_26_drive_0002_sync/image_00/data/*.png"};
   std::vector<std::string> paths = Edrak::IO::GetFiles(glob_pattern);
@@ -27,7 +27,7 @@ TEST_CASE("correct files are returned", "[Edrak::IO::GetFiles]") {
 
 // This tests the correct out_of_range exceptions are generated
 TEST_CASE("NextFrames returning correct number of frames",
-          "[Edrak::IO::MonoReader::nextFrame]") {
+          "Edrak::IO::MonoReader::nextFrame") {
   Edrak::IO::MonoReader reader{
       "../tests/2011_09_26/2011_09_26_drive_0002_sync/image_00/data/*.png",
       Edrak::IO::ImageType::GRAY, false};
@@ -41,7 +41,7 @@ TEST_CASE("NextFrames returning correct number of frames",
 }
 
 TEST_CASE("NextFrames returning correct number of frames(RGB)",
-          "[Edrak::IO::MonoReader::nextFrame]") {
+          "Edrak::IO::MonoReader::nextFrame") {
   Edrak::IO::MonoReader reader{
       "../tests/2011_09_26/2011_09_26_drive_0002_sync/image_02/data/*.png",
       Edrak::IO::ImageType::RGB, false};

8280ae63ac1e9a392536c5d2f2661f29a25f4122
Ibrahim Essam <ibrahim.essam@avelabs.com>
2022-04-09 14:55:34 +0100

    Orb Features

---
+++ Edrak/include/Edrak/Benchamrk/Timer.hpp
@@ -0,0 +1,42 @@
+#include <cassert>
+#include <chrono>
+#include <iostream>
+#include <string>
+namespace Edrak {
+namespace Benchmark {
+class Timer {
+public:
+  Timer(const std::string text)
+      : text_(text), t_start_(std::chrono::steady_clock::now()) {}
+
+  void Stop() {
+
+    assert(stoped_);
+    t_end_ = std::chrono::steady_clock::now();
+    t_elapsed_ = std::chrono::duration_cast<std::chrono::duration<double>>(
+        t_end_ - t_start_);
+    std::cout << "======= Timing " << text_ << "Took " << t_elapsed_.count()
+              << " Seconds ========" << std::endl;
+    stoped_ = true;
+  }
+  void Restart(const std::string text = "") {
+    if (!text.empty()) {
+      text_ = text;
+    }
+    t_start_ = std::chrono::steady_clock::now();
+    stoped_ = false;
+  }
+  ~Timer() {
+    if (!stoped_)
+      Stop();
+  }
+
+private:
+  std::string text_;
+  bool stoped_ = false;
+  std::chrono::steady_clock::time_point t_start_;
+  std::chrono::steady_clock::time_point t_end_;
+  std::chrono::duration<double> t_elapsed_;
+};
+} // namespace Benchmark
+} // namespace Edrak
---
+++ tests/3rdparty/opencvOrb.cpp
@@ -0,0 +1,72 @@
+#include "../catch.hpp"
+#include "Edrak/Benchamrk/Timer.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+TEST_CASE("OpenCv_Features", "OrbFeatures") {
+  std::string data_dir = EDRAK_TEST_DATA_DIR;
+  cv::Mat im1 = cv::imread(data_dir + "Images/Orb/0000000026.png");
+  cv::Mat im2 = cv::imread(data_dir + "Images/Orb/0000000027.png");
+
+  std::vector<cv::KeyPoint> keyPoints1, keyPoints2;
+  cv::Mat descriptors1, descriptors2;
+
+  cv::Ptr<cv::FeatureDetector> detector = cv::ORB::create();
+  cv::Ptr<cv::DescriptorExtractor> descriptor = cv::ORB::create();
+  cv::Ptr<cv::DescriptorMatcher> matcher =
+      cv::DescriptorMatcher::create("BruteForce-Hamming");
+
+  Edrak::Benchmark::Timer tOrbFeatures("Orb Featueres");
+  // Detect Keypoints
+  Edrak::Benchmark::Timer tKeypoints("ORB KeyPoints");
+  detector->detect(im1, keyPoints1);
+  detector->detect(im2, keyPoints2);
+  tKeypoints.Stop();
+
+  // Compute BRIEF Descriptors
+  Edrak::Benchmark::Timer tDescriptors("ORB Descriptors");
+  descriptor->compute(im1, keyPoints1, descriptors1);
+  descriptor->compute(im2, keyPoints2, descriptors2);
+  tDescriptors.Stop();
+
+  tOrbFeatures.Stop();
+
+  // Draw KeyPoints
+  cv::Mat outIm1;
+  cv::drawKeypoints(im1, keyPoints1, outIm1);
+  //   cv::imshow("Orb Features", outIm1);
+  //   cv::waitKey(0);
+
+  // Match Keypoints
+  std::vector<cv::DMatch> matches;
+  Edrak::Benchmark::Timer tMatchDescriptors("Matching ORB Descriptors");
+  matcher->match(descriptors1, descriptors2, matches);
+  tMatchDescriptors.Stop();
+
+  const auto [min, max] = std::minmax_element(
+      matches.begin(), matches.end(),
+      [](const cv::DMatch &first, const cv::DMatch &second) {
+        return first.distance < second.distance;
+      });
+
+  std::cout << "Max Distance " << max->distance << std::endl;
+  std::cout << "Min Distance " << min->distance << std::endl;
+
+  float filerDist = (max->distance - min->distance) * 0.30;
+  // Filter Bad matches
+  std::vector<cv::DMatch> filteredMatches;
+  for (size_t i = 0; i < descriptors1.rows; i++) {
+    if (matches[i].distance < filerDist)
+      filteredMatches.push_back(matches[i]);
+  }
+
+  // Draw Matches
+  cv::Mat imallMatches, imfilteredMatches;
+  cv::drawMatches(im1, keyPoints1, im2, keyPoints2, matches, imallMatches);
+  cv::drawMatches(im1, keyPoints1, im2, keyPoints2, filteredMatches,
+                  imfilteredMatches);
+  cv::imshow("all matches", imallMatches);
+  cv::imshow("Filtered matches", imfilteredMatches);
+  cv::waitKey(0);
+}

260a7f27a44225881eac1b5757416779683d69c3
Ibrahim <ibrahim.essam1995@gmail.com>
2023-01-22 21:01:11 +0000

    update VO example

--- Examples/VO/test_PoseEstmation.cpp
+++ Examples/VO/test_PoseEstmation.cpp
@@ -5,24 +5,22 @@
 
 int main(int argc, char const *argv[]) {
   std::string data_dir = EDRAK_TEST_DATA_DIR;
-  std::string imgs_path = data_dir + "KITTI/VO/16";
+  std::string imgs_path = data_dir + "KITTI/2011_09_26_drive_0064_sync";
+  Edrak::CameraModel leftCamCalib(721.5377,721.5377,609.5593, 172.8540, Sophus::SE3d());
+  Edrak::VO::PoseEstmation pe(leftCamCalib.calibration);
 
-  std::vector<Edrak::CameraModel> camerasCalib =
-      Edrak::ParseKITTICameras(imgs_path + "/calib.txt");
-  Edrak::StereoCamera cam{camerasCalib[0], camerasCalib[1],
-                          camerasCalib[1].pose.translation().norm()};
-
-  Edrak::VO::PoseEstmation pe(camerasCalib[0].calibration);
-
-  Edrak::IO::MonoReader reader{imgs_path + "/image_0/*.png",
+  Edrak::IO::MonoReader reader{imgs_path + "/image_00/data/*.png",
                                Edrak::IO::ImageType::GRAY, false};
   cv::Mat frame;
   Edrak::TrajectoryD trajectory;
-  for (size_t i = 0; i < 10; i++) {
+  Edrak::SE3D lastPose;
+  for (size_t i = 0; i < 500; i++) {
     reader.NextFrame(frame);
     pe.Process(frame);
     std::cout << "Pose" << i << pe.Pose().matrix() << std::endl;
-    trajectory.push_back(pe.Pose());
+    Edrak::SE3D Twc = lastPose * pe.Pose();
+    trajectory.push_back(Twc);
+    lastPose = Twc;
   }
   Edrak::Visual::DrawTrajectory(trajectory);
   return 0;

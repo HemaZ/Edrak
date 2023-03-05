a84e86bf2e9a4d8e1f0dcce73e5e7ef0b6cf992e
Ibrahim <ibrahim.essam1995@gmail.com>
2023-03-05 13:10:59 +0000

    Add Export Trajceotry function

--- Edrak/include/Edrak/IO/Trajectory.hpp
+++ Edrak/include/Edrak/IO/Trajectory.hpp
@@ -6,7 +6,26 @@
 #include <sophus/se3.hpp>
 namespace Edrak {
 namespace IO {
-Edrak::TrajectoryD LoadTrajectory(const std::string path);
+/**
+ * @brief Load trajectory from CSV file. each row should has timestamp, x, y, z,
+ *  quaterion_x,quaterion_y,quaterion_z, quaterion_w
+ *
+ * @param path CSV file path.
+ * @return Edrak::TrajectoryD Trajectory as SE3D poses.
+ */
+Edrak::TrajectoryD LoadTrajectory(const std::string &path);
+
+/**
+ * @brief Export trajectory as TUM file format.
+ *
+ * @param path TUM file path to write the trajectory to.
+ * @param trajectory Trajectory to Save.
+ * @param writeHeader Save header to the trajectory file.
+ * @return bool If success.
+ */
+bool ExportTrajectory(const std::string &path,
+                      const Edrak::TrajectoryD &trajectory,
+                      bool writeHeader = false);
 } // namespace IO
 
 } // namespace Edrak
